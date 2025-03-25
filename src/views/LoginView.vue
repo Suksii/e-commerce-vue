95f8ae8d7c0c9ff69c78cfd015f25f0ef0a4b118
unknown <ramovic225@gmail.com>
2025-03-25 14:35:32 +0100

    login view styled

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useRouter } from 'vue-router'
+import cartImage from '@/assets/cartImage.jpg'
 
 const router = useRouter()
 
@@ -10,44 +11,50 @@ function handleLogin() {
 
 <template>
   <div
-    class="min-h-screen w-full relative"
+    class="min-h-screen w-full relative bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 flex justify-center items-center"
   >
-    <div class="max-w-96 h-fit shadow-2xl p-8 rounded-xl translate-full relative">
-      <div class="absolute inset-0 blur-xl"></div>
-      <div class="flex flex-col justify-center items-center relative">
-        <h2 class="text-3xl font-semibold italic pb-6">Login</h2>
-        <p class="pb-4">
+    <div
+      class="max-w-2xl min-h-96 h-fit shadow-2xl p-8 rounded-xl w-full flex flex-col md:flex-row gap-6 bg-white/90"
+    >
+      <div class="flex flex-col justify-center items-center flex-1 space-y-6">
+        <h2 class="text-4xl font-semibold italic">Login</h2>
+        <p class="text-gray-700 text-lg">
           Don't have an account?
-          <span class="text-blue-700 italic cursor-pointer" @click="router.push('/register')"
+          <span
+            class="text-teal-600 hover:text-teal-500 transition duration-300 italic cursor-pointer"
+            @click="router.push('/register')"
             >Register now</span
           >
         </p>
         <form
-          class="flex flex-col items-center justify-center gap-2.5 w-full"
+          class="flex flex-col items-center justify-center gap-4 w-full"
           @submit.prevent="handleLogin"
         >
           <input
             placeholder="Type username"
-            class="py-2 px-3 bg-gray-200 rounded-md w-full outline-none"
+            class="py-3 px-4 bg-gray-200 rounded-md w-full outline-none focus:ring-2 placeholder:text-gray-500 focus:ring-teal-500"
           />
           <input
             placeholder="Type password"
-            class="py-2 px-3 bg-gray-200 rounded-md w-full outline-none"
+            class="py-3 px-4 bg-gray-200 rounded-md w-full outline-none focus:ring-2 placeholder:text-gray-500 focus:ring-teal-500"
           />
           <button
-            class="w-full py-2 px-3 rounded-md cursor-pointer bg-cyan-900 relative group overflow-hidden"
+            class="w-full py-3 px-4 rounded-md cursor-pointer bg-teal-600 relative group overflow-hidden"
           >
             <span
-              class="absolute inset-0 w-16 h-16 bg-cyan-600 opacity-50 blur-md rotate-45 transition-all duration-500 ease-out group-hover:w-[300%] group-hover:h-[300%]"
+              class="absolute inset-0 w-16 h-16 bg-teal-400 opacity-50 blur-md rotate-45 transition-all duration-500 ease-out group-hover:w-[300%] group-hover:h-[300%]"
             ></span>
             <span
-              class="absolute inset-0 w-12 h-12 bg-cyan-400 opacity-30 blur-lg rotate-45 transition-all duration-700 ease-out group-hover:w-[250%] group-hover:h-[250%]"
+              class="absolute inset-0 w-12 h-12 bg-teal-200 opacity-30 blur-lg rotate-45 transition-all duration-700 ease-out group-hover:w-[250%] group-hover:h-[250%]"
             ></span>
             <span class="relative text-xl text-white uppercase tracking-wider font-semibold z-10"
               >Login</span
             >
           </button>
         </form>
+      </div>
+      <div class="flex-1 hidden md:block">
+        <img :src="cartImage" class="object-cover rounded-xl w-full" />
       </div>
     </div>
   </div>
