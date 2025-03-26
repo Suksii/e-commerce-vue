e43467fe2035fffbaae940c1a618b9eb8f133b3f
unknown <ramovic225@gmail.com>
2025-03-26 23:42:20 +0100

    profile content styled

--- src/components/ProfileContent.vue
+++ src/components/ProfileContent.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { Icon } from '@iconify/vue'
+
 const userMail = 'example@gmail.com'
 const userName = 'user'
 </script>
@@ -8,7 +10,7 @@ const userName = 'user'
     <h3 class="text-2xl font-semibold pb-6">Hello, {{ userName }}</h3>
     <div class="flex flex-col gap-1">
       <div class="flex justify-between items-center">
-        <label>Email</label>
+        <label class="text-gray-600 font-medium">Email</label>
         <input
           disabled
           :placeholder="userMail"
@@ -16,21 +18,30 @@ const userName = 'user'
         />
       </div>
       <div class="flex justify-between items-center">
-        <label>Username</label>
+        <label class="text-gray-600 font-medium">Username</label>
         <input
           :placeholder="userName"
           class="p-2 bg-gray-200 w-[220px] rounded-md outline-none focus:ring-2 placeholder:text-gray-500 focus:ring-teal-500 transition duration-200"
         />
       </div>
       <div class="flex justify-between items-center gap-1 pt-4">
-        <div class="">
-          <button class="bg-red-600 p-2 rounded-md text-white cursor-pointer">
-            Delete account
+        <button
+          class="bg-red-500/20 px-4 py-2 rounded-md cursor-pointer text-red-800 font-medium shadow-md flex items-center gap-2 transition-all duration-200 hover:bg-red-600/90 hover:text-white hover:shadow-lg"
+        >
+          <Icon icon="mynaui:trash" width="20" height="20" />
+          Delete user
+        </button>
+        <div class="flex gap-3">
+          <button
+            class="min-w-24 border-2 border-gray-500 px-4 py-2 rounded-md cursor-pointer text-gray-700 font-medium shadow-md transition-all duration-200 hover:bg-gray-600 hover:text-white hover:border-gray-700"
+          >
+            Close
           </button>
-        </div>
-        <div class="flex gap-2">
-          <button class="bg-neutral-500 p-2 rounded-md text-white cursor-pointer">Close</button>
-          <button class="bg-teal-700 p-2 rounded-md text-white cursor-pointer">Save changes</button>
+          <button
+            class="min-w-24 bg-teal-600 px-4 py-2 rounded-md cursor-pointer text-white font-medium shadow-md transition-all duration-200 hover:bg-teal-700 hover:shadow-lg"
+          >
+            Save changes
+          </button>
         </div>
       </div>
     </div>
