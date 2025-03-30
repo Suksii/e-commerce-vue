802bba2af22c2b1a964d9ed685dde034a9980a05
unknown <ramovic225@gmail.com>
2025-03-30 01:24:32 +0100

    Notification component created

---
+++ src/components/Notification.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({ showNotification: Boolean })
+</script>
+
+<template>
+  <Transition
+    enter-active-class="transition-all duration-1000"
+    leave-active-class="transition-all duration-1000"
+    enter-to-class="opacity-100 translate-y-0"
+    enter-from-class="opacity-0 translate-y-full"
+    leave-from-class="opacity-100 translate-y-0"
+    leave-to-class="opacity-0 -translate-y-full"
+    mode="out-in"
+  >
+    <div
+      v-if="showNotification"
+      class="fixed bottom-0 right-0 m-6 bg-white px-12 py-4 rounded-md shadow-[0px_10px_20px_rgba(0,0,0,0.3)]"
+    >
+      <p>Notification Message</p>
+    </div>
+  </Transition>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup>
 import CardList from '@/components/CardList.vue'
+import Notification from '@/components/Notification.vue'
+import { ref } from 'vue'
+const showNotification = ref(false)
+
+const show = () => {
+  showNotification.value = true
+  setTimeout(() => {
+    showNotification.value = false
+  }, 2000)
+}
 </script>
 
 <template>
   <div class="flex justify-center items-center h-screen">
     <CardList />
+    <button @click="show">Show nottification</button>
+    <Notification :showNotification="showNotification" />
   </div>
 </template>
