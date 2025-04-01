23c9b5c7f2faef6b492ef807dde117b506ec96e4
unknown <ramovic225@gmail.com>
2025-04-01 21:56:31 +0200

    server created and db connected

---
+++ client/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import CardList from '@/components/CardList.vue'
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen"> 
+    <CardList />
+  </div>
+</template>
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error("Connection failed:", err));
+
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
