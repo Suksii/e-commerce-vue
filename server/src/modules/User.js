b1bdeae2b62495bd2819431b6ac995d1389cd986
unknown <ramovic225@gmail.com>
2025-04-01 22:22:10 +0200

    User Schema created

--- server/src/modules/User.js
+++ server/src/modules/User.js
@@ -1,1 +1,8 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
