627c1afbe9a4e56b54a585034059c6c8026faa29
unknown <ramovic225@gmail.com>
2025-04-02 12:09:58 +0200

    login with token created

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,5 +1,6 @@
 import { User } from "../modules/User.js";
-import bycrypt from "bcryptjs";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   const { username, email, password, confirmPassword } = req.body;
@@ -15,13 +16,13 @@ export const registerUser = async (req, res) => {
     if (password !== confirmPassword) {
       return res.status(400).json({ message: "Passwords do not match" });
     }
-    const salt = bycrypt.genSaltSync(10);
-    const hashedPassword = bycrypt.hashSync(password, salt);
+    const salt = bcrypt.genSaltSync(10);
+    // const hashedPassword = bcrypt.hashSync(password, salt);
 
     const newUser = await User.create({
       username,
       email,
-      password: hashedPassword,
+      password: bcrypt.hashSync(password, salt),
     });
     res.status(201).send({ newUser, message: "User created successfully" });
   } catch (error) {
@@ -37,10 +38,29 @@ export const loginUser = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "Username does not exist" });
     }
-    if (user.password !== password) {
+    const passMatch = bcrypt.compareSync(password, user.password);
+
+    if (!passMatch) {
       return res.status(406).json({ message: "Password is not valid" });
     }
-    res.status(201).send({ user });
+
+    jwt.sign(
+      {
+        email: user.email,
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      {},
+      (err, token) => {
+        if (err) {
+          res.status(422).json(err);
+        } else {
+          res
+            .cookie("token", token)
+            .json({ user, message: "Login successful" });
+        }
+      }
+    );
   } catch (error) {
     res.status(422).json(error);
   }
