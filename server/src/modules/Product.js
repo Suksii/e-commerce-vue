870c1d4b7ffc3b32217948aef26d80c6cce9d5bb
unknown <ramovic225@gmail.com>
2025-04-04 20:45:14 +0200

    Product model created

---
+++ server/src/modules/Product.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: String, required: true },
+  discount: { type: Number },
+  category: { type: String, required: true },
+  image: { type: String, required: true },
+});
+
+export const Product = mongoose.model("product", ProductSchema);
