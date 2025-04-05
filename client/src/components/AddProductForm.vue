2be24c317e7ac1d4fe6cbd0cf6d7d7425aee3396
unknown <ramovic225@gmail.com>
2025-04-05 03:14:09 +0200

    addProductForm styled

--- client/src/components/AddProductForm.vue
+++ client/src/components/AddProductForm.vue
@@ -27,18 +27,33 @@ async function addProduct() {
 </script>
 
 <template>
-  <div class="w-[80%] mx-auto py-24">
+  <div class="w-[95%] md:w-[50%] mx-auto py-24">
     <h2 class="text-center text-4xl">Add new product</h2>
     <form
       @submit.prevent="addProduct"
       class="flex flex-col items-center justify-center w-full gap-4 py-12"
     >
-      <input class="custom-input w-full py-3 px-4" v-model="productData.name" />
-      <input class="custom-input w-full py-3 px-4" v-model="productData.description" />
-      <input class="custom-input w-full py-3 px-4" v-model="productData.price" />
-      <input class="custom-input w-full py-3 px-4" v-model="productData.category" />
-      <input class="custom-input w-full py-3 px-4" v-model="productData.image" />
-      <button class="save-button">Add Product</button>
+      <div class="flex flex-col w-full">
+        <label class="text-xl font-medium">Name</label>
+        <input class="custom-input w-full py-3 px-4" v-model="productData.name" />
+      </div>
+      <div class="flex flex-col w-full">
+        <label class="text-xl font-medium">Description</label>
+        <input class="custom-input w-full py-3 px-4" v-model="productData.description" />
+      </div>
+      <div class="flex flex-col w-full">
+        <label class="text-xl font-medium">Price</label>
+        <input class="custom-input w-full py-3 px-4" v-model="productData.price" />
+      </div>
+      <div class="flex flex-col w-full">
+        <label class="text-xl font-medium">Category</label>
+        <input class="custom-input w-full py-3 px-4" v-model="productData.category" />
+      </div>
+      <div class="flex flex-col w-full">
+        <label class="text-xl font-medium">Image</label>
+        <input class="custom-input w-full py-3 px-4" v-model="productData.image" />
+      </div>
+      <button class="min-w-42 w-full my-4 h-12 save-button">Add Product</button>
     </form>
   </div>
 </template>
