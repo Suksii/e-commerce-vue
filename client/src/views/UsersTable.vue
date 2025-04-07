52ce7c0e4829a9516d0970d9050fcfe2d2a181e3
unknown <ramovic225@gmail.com>
2025-04-07 13:40:16 +0200

    users displayed in table

--- client/src/views/UsersTable.vue
+++ client/src/views/UsersTable.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { request } from '@/api'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+
+const usersData = ref([])
 
 onMounted(async () => {
   try {
     const { data } = await request.get('/api/users/get-users')
+    usersData.value = data
     console.log(data)
   } catch (error) {
     console.error(error)
@@ -13,5 +16,30 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>Hello</div>
+  <div class="overflow-x-auto w-[95%] md:w-[80%] h-fit mx-auto my-12">
+    <table class="overflow-x-hidden w-full table-auto border-collapse border border-gray-200">
+      <thead class="w-full bg-teal-600 text-white">
+        <th>#</th>
+        <th>User ID</th>
+        <th>Username</th>
+        <th>Email</th>
+      </thead>
+      <tbody class="w-full">
+        <tr v-for="(user, index) of usersData" class="text-center even:bg-teal-100">
+          <td>{{ index + 1 }}.</td>
+          <td>{{ user._id }}</td>
+          <td>{{ user.username }}</td>
+          <td>{{ user.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+<style scoped>
+td,
+th {
+  font-size: 24px;
+  padding: 16px 12px;
+  border: 1px solid #e5e7eb;
+}
+</style>
