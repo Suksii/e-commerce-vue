7cdeec25808b4380b44c2d9280a23f5064421aba
unknown <ramovic225@gmail.com>
2025-04-08 15:03:21 +0200

    get-carts endpoint created

--- server/src/routes/CartRoute.js
+++ server/src/routes/CartRoute.js
@@ -4,5 +4,6 @@ import { addCart } from "../controllers/CartController.js";
 const router = Router();
 
 router.post("/add-cart/:id", addCart);
+router.get("/get-carts", addCart);
 
 export const cartRoute = router;
