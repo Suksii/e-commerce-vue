bca5ab30257d1b7a6e5de451bd2268e3dacb5131
unknown <ramovic225@gmail.com>
2025-04-09 07:11:39 +0200

    delete-cart endpoint created

--- server/src/routes/CartRoute.js
+++ server/src/routes/CartRoute.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { addCart, getCarts } from "../controllers/CartController.js";
+import {
+  addCart,
+  deleteCart,
+  getCarts,
+} from "../controllers/CartController.js";
 
 const router = Router();
 
 router.post("/add-cart/:id", addCart);
 router.get("/carts", getCarts);
+router.delete("/delete-cart/:id", deleteCart);
 
 export const cartRoute = router;
