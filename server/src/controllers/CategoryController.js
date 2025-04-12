36e4e8dab79a765c50ad65f570a97357fa1d5c65
unknown <ramovic225@gmail.com>
2025-04-12 20:03:49 +0200

    addCategory controller created

---
+++ server/src/controllers/CategoryController.js
@@ -0,0 +1,20 @@
+import { Category } from "../models/Category";
+
+export const addCategory = async (req, res) => {
+  try {
+    const { name, image, parentCategory, slug, gender, season } = req.body;
+    const newCategory = await Category.create({
+      name,
+      image,
+      parentCategory,
+      slug,
+      gender,
+      season,
+    });
+    res
+      .status(200)
+      .json({ message: "Category Successfully added", category: newCategory });
+  } catch (error) {
+    res.json({ message: "Internal Server Error" });
+  }
+};
