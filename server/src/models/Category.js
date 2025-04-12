5aaf11e48b213808c895e151eb315a4f54ed4183
unknown <ramovic225@gmail.com>
2025-04-12 22:57:12 +0200

    SubCategory model created

--- server/src/models/Category.js
+++ server/src/models/Category.js
@@ -1,8 +1,18 @@
 import mongoose from "mongoose";
 
+const subCategorySchema = new mongoose.Schema({
+  name: { type: String },
+});
+
 const CategorySchema = new mongoose.Schema({
   name: { type: String, required: true },
   slug: { type: String, required: true },
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Unisex"],
+  },
+  season: { type: String, enum: ["Summer", "Winter", "Autumn", "Spring"] },
+  image: String,
 });
 
 export const Category = mongoose.model("Category", CategorySchema);
