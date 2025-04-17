4e44de937d44cb3f5c0bea841fb2c83f8b69b278
unknown <ramovic225@gmail.com>
2025-04-17 20:23:58 +0200

    category controller and model changed

--- server/src/controllers/CategoryController.js
+++ server/src/controllers/CategoryController.js
@@ -2,11 +2,11 @@ import { Category } from "../models/Category.js";
 
 export const addCategory = async (req, res) => {
   try {
-    const { name, image, subCategories, slug, gender, season } = req.body;
+    const { name, image, parentCategory, slug, gender, season } = req.body;
     const newCategory = await Category.create({
       name,
       image,
-      subCategories,
+      parentCategory,
       slug,
       gender,
       season,
--- server/src/models/Category.js
+++ server/src/models/Category.js
@@ -1,20 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 
 const CategorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  slug: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
+  slug: { type: String, required: true, unique: true },
   gender: {
     type: String,
     enum: ["Male", "Female", "Unisex"],
   },
   season: { type: String, enum: ["Summer", "Winter", "Autumn", "Spring"] },
   image: String,
-  subCategories: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "SubCategory",
-    },
-  ],
+  parentCategory: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    default: null,
+  },
 });
 
 export const Category = mongoose.model("Category", CategorySchema);
