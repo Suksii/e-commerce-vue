daf37b522ddbfc1018661fd3e6bd01528e3038c0
unknown <ramovic225@gmail.com>
2025-05-03 19:05:05 +0200

    no data message added

--- client/src/components/CardList.vue
+++ client/src/components/CardList.vue
@@ -7,10 +7,9 @@ defineProps({
 </script>
 
 <template>
-  <div
-    class="flex justify-center flex-wrap gap-4 py-12"
-  >
+  <div class="flex justify-center flex-wrap gap-4 py-12">
     <div
+      v-if="data && data.length"
       v-for="item of data"
       :key="data._id"
       class="flex justify-center gap-4"
@@ -25,5 +24,6 @@ defineProps({
         :discount="item.discount"
       />
     </div>
+    <div v-else class="text-3xl font-medium tracking-wide">No data to display</div>
   </div>
 </template>
