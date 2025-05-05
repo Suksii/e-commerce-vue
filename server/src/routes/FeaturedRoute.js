9bb28bcb3fd2a5f3112cdd075e63ceef9be288d8
unknown <ramovic225@gmail.com>
2025-05-05 12:53:12 +0200

    getFeatured endpoint created

--- server/src/routes/FeaturedRoute.js
+++ server/src/routes/FeaturedRoute.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { addFeatured } from "../controllers/FeaturedController.js";
+import {
+  addFeatured,
+  deleteFeatured,
+  getFeatured,
+  updateFeatured,
+} from "../controllers/FeaturedController.js";
 
 const router = Router();
 
 router.post("/add", addFeatured);
+router.put("/update/:id", updateFeatured);
+router.delete("/delete/:id", deleteFeatured);
+router.get("/", getFeatured);
 
 export const featuredRoute = router;
