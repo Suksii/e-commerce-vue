83f7ef4f37af24f703fdd04096036464e63254f0
unknown <ramovic225@gmail.com>
2025-05-07 22:52:15 +0200

    FeaturedSlider style modified

--- client/src/components/FeaturedSection.vue
+++ client/src/components/FeaturedSection.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useFeaturedStore } from '@/stores/featured'
 import { Icon } from '@iconify/vue'
-import { onMounted, onUnmounted, ref, watchEffect } from 'vue'
+import { onMounted, onUnmounted, ref, watch, watchEffect } from 'vue'
 
 const featuredStore = useFeaturedStore()
 const currentIndex = ref(0)
@@ -19,10 +19,15 @@ const prev = () => {
 }
 
 const slideNext = () => {
+  clearInterval(interval)
   interval = setInterval(() => {
     next()
   }, 5000)
 }
+
+watch(currentIndex, () => {
+  slideNext()
+})
 
 onMounted(async () => {
   await featuredStore.getFeatured()
@@ -38,36 +43,46 @@ onUnmounted(() => {
   <div class="relative w-full">
     <div class="flex w-full overflow-hidden relative">
       <div
-        v-for="featured of featuredStore.featuredData"
-        class="flex items-center w-full h-[500px] relative shrink-0 transition duration-300"
-        :style="{ transform: `translateX(-${currentIndex * 100}%)` }"
+        class="flex transition-transform duration-500 ease-in-out"
+        :style="{
+          transform: `translateX(-${currentIndex * 100}%)`,
+          width: `${featuredStore.featuredData.length * 100}%`,
+        }"
       >
-        <div class="flex items-center justify-center flex-1 h-full bg-gray-100">
-          <div class="flex flex-col items-center justify-center">
-            <h2 class="text-2xl font-medium">{{ featured.title }}</h2>
-            <p class="">{{ featured.description }}</p>
+        <div
+          v-for="featured in featuredStore.featuredData"
+          :key="featured._id"
+          class="flex items-center w-full h-auto max-h-[500px] relative shrink-0"
+        >
+          <div class="flex items-center justify-center flex-1 h-full bg-gray-100">
+            <div class="flex flex-col items-center justify-center h-fit">
+              <h2 class="text-2xl font-medium">{{ featured.title }}</h2>
+              <p>{{ featured.description }}</p>
+            </div>
           </div>
-        </div>
-        <div class="flex-2">
-          <img
-            :src="'http://localhost:3000/uploads/featured/' + featured.image"
-            :alt="featured.name"
-            class="text-center w-full object-cover"
-          />
+          <div class="flex-2 w-full h-full">
+            <img
+              :src="'http://localhost:3000/uploads/featured/' + featured.image"
+              :alt="featured.name"
+              class="w-full h-full object-cover object-center"
+            />
+          </div>
         </div>
       </div>
     </div>
     <div class="absolute top-1/2 -translate-y-1/2 right-0 p-12 flex flex-col">
       <button
         v-for="(_, index) in featuredStore.featuredData"
+        :key="index"
         @click="currentIndex = index"
-        class="cursor-pointer w-full"
+        class="cursor-pointer"
       >
         <Icon
           icon="pepicons-pop:line-x"
           width="30"
           height="30"
-          :class="{ 'text-teal-600': currentIndex === index }"
+          class="transition duration-300"
+          :class="currentIndex === index ? 'text-teal-600' : 'text-gray-400 hover:text-gray-600'"
         />
       </button>
     </div>
