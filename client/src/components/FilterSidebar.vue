445984e874d61a092cf18dbdc99dbcee359b4cf4
unknown <ramovic225@gmail.com>
2025-05-17 12:32:59 +0200

    SingleFilter component created and used

--- client/src/components/FilterSidebar.vue
+++ client/src/components/FilterSidebar.vue
@@ -6,6 +6,7 @@ import CustomCheckBox from './CustomCheckBox.vue'
 import { useProductsStore } from '@/stores/products'
 import { Icon } from '@iconify/vue'
 import { useValidation } from '@/composables/useValidation'
+import SingleFilter from './SingleFilter.vue'
 
 const brandStore = useBrandStore()
 const categoryStore = useCategoryStore()
@@ -57,110 +58,56 @@ const rangeBackground = computed(() => {
 
 <template>
   <div class="w-64">
-    <div class="flex flex-col w-full gap-2">
-      <div v-if="categoryStore.childCategoriesData.length" class="">
-        <div class="flex justify-between items-center w-full border-b-2 border-teal-600 mb-2">
-          <h3 class="text-sm font-medium uppercase">Categories</h3>
-          <Icon
-            :icon="expand.categories ? 'line-md:minus' : 'line-md:plus'"
-            width="22"
-            height="22"
-            class="text-teal-700 cursor-pointer transition"
-            @click="handleExpand('categories')"
+    <div class="flex flex-col w-full gap-1">
+      <SingleFilter name="Categories" item="categories">
+        <div
+          v-if="categoryStore.childCategoriesData.length"
+          v-for="category in categoryStore.childCategoriesData"
+          :key="category._id"
+          class="flex items-center gap-2"
+        >
+          <CustomCheckBox
+            v-model:selectedItem="productsStore.selectedCategories"
+            :name="category.name"
+            :item="category"
           />
         </div>
-        <div
-          :class="expand.categories ? 'max-h-96' : 'max-h-0'"
-          class="overflow-hidden transition-all duration-500"
-        >
-          <div
-            v-for="category in categoryStore.childCategoriesData"
-            :key="category._id"
-            class="flex items-center gap-2"
-          >
-            <CustomCheckBox
-              v-model:selectedItem="productsStore.selectedCategories"
-              :name="category.name"
-              :item="category"
+      </SingleFilter>
+      <SingleFilter name="Brands" item="brands">
+        <div v-for="brand in brandStore.brandData" :key="brand._id" class="flex items-center gap-2">
+          <CustomCheckBox
+            v-model:selectedItem="productsStore.selectedBrands"
+            :name="brand.name"
+            :item="brand"
+          />
+        </div>
+      </SingleFilter>
+      <SingleFilter name="Price" item="price">
+        <div class="flex flex-col">
+          <div class="relative w-full mt-2">
+            <input
+              type="range"
+              :min="minPrice"
+              :max="maxPrice"
+              v-model="productsStore.selectedMin"
+              @input="validateMin"
+              class="input-range absolute top-0 left-0 w-full z-40"
+              :style="rangeBackground"
+            />
+            <input
+              type="range"
+              :min="minPrice"
+              :max="maxPrice"
+              @input="validateMax"
+              v-model="productsStore.selectedMax"
+              class="input-range absolute top-0 left-0 w-full z-50"
             />
           </div>
         </div>
-      </div>
-      <div v-if="brandStore.brandData.length">
-        <div class="flex justify-between items-center w-full border-b-2 border-teal-600 mb-2">
-          <h3 class="text-sm font-medium uppercase">Brands</h3>
-          <Icon
-            :icon="expand.brands ? 'line-md:minus' : 'line-md:plus'"
-            width="22"
-            height="22"
-            class="text-teal-700 cursor-pointer"
-            @click="handleExpand('brands')"
-          />
-        </div>
-
-        <div
-          @mouseenter="isOverflowVisible = true"
-          @mouseleave="isOverflowVisible = false"
-          :class="[
-            expand.brands ? 'max-h-96' : 'max-h-0',
-            isOverflowVisible ? 'overflow-auto' : 'overflow-hidden',
-          ]"
-          class="transition-all duration-500"
-        >
-          <div
-            v-for="brand in brandStore.brandData"
-            :key="brand._id"
-            class="flex items-center gap-2"
-          >
-            <CustomCheckBox
-              v-model:selectedItem="productsStore.selectedBrands"
-              :name="brand.name"
-              :item="brand"
-            />
-          </div>
-        </div>
-      </div>
-      <div class="w-full">
-        <div class="flex justify-between items-center w-full border-b-2 border-teal-600 mb-2">
-          <h3 class="text-sm font-medium uppercase">Price</h3>
-          <Icon
-            :icon="expand.price ? 'line-md:minus' : 'line-md:plus'"
-            width="22"
-            height="22"
-            class="text-teal-700 cursor-pointer"
-            @click="handleExpand('price')"
-          />
-        </div>
-        <div
-          :class="expand.price ? 'max-h-96' : 'max-h-0'"
-          class="overflow-hidden transition-all duration-500"
-        >
-          <div class="flex flex-col">
-            <div class="relative w-full mt-2">
-              <input
-                type="range"
-                :min="minPrice"
-                :max="maxPrice"
-                v-model="productsStore.selectedMin"
-                @input="validateMin"
-                class="input-range absolute top-0 left-0 w-full z-40"
-                :style="rangeBackground"
-              />
-              <input
-                type="range"
-                :min="minPrice"
-                :max="maxPrice"
-                @input="validateMax"
-                v-model="productsStore.selectedMax"
-                class="input-range absolute top-0 left-0 w-full z-50"
-              />
-            </div>
-          </div>
-          <p class="font-medium pt-4">
-            {{ productsStore.selectedMin }}€ - {{ productsStore.selectedMax }}€
-          </p>
-        </div>
-      </div>
+        <p class="font-medium pt-4">
+          {{ productsStore.selectedMin }}€ - {{ productsStore.selectedMax }}€
+        </p>
+      </SingleFilter>
     </div>
   </div>
 </template>
---
+++ client/src/components/SingleFilter.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+import { reactive, ref } from 'vue'
+
+defineProps({
+  name: String,
+  item: String,
+})
+
+const expand = reactive({})
+const isOverflowVisible = ref(false)
+
+const handleExpand = (item) => {
+  expand[item] = !expand[item]
+}
+</script>
+
+<template>
+  <div class="flex justify-between items-center w-full border-b-2 border-teal-600 mb-2">
+    <h3 class="text-sm font-medium uppercase">{{ name }}</h3>
+    <Icon
+      :icon="expand[item] ? 'line-md:minus' : 'line-md:plus'"
+      width="22"
+      height="22"
+      class="text-teal-700 cursor-pointer"
+      @click="handleExpand(item)"
+    />
+  </div>
+
+  <div
+    @mouseenter="isOverflowVisible = true"
+    @mouseleave="isOverflowVisible = false"
+    :class="[
+      expand[item] ? 'max-h-96' : 'max-h-0',
+      isOverflowVisible ? 'overflow-auto' : 'overflow-hidden',
+    ]"
+    class="transition-all duration-500"
+  >
+    <slot></slot>
+  </div>
+</template>
