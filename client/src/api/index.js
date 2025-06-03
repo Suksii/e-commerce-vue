35951b70544d8af6580f905c3a50dcebcb9e864a
Ramović Šućo <ramovic225@gmail.com>
2025-06-03 23:43:05 +0200

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,5 +2,6 @@ import axios from 'axios'
 
 export const request = axios.create({
   baseURL: `https://e-commerce-vue-server.vercel.app`,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   withCredentials: true,
 })
