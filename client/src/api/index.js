7522e80e3987b0d4a4690c8f2be6a6d58808d796
unknown <ramovic225@gmail.com>
2025-06-03 21:10:07 +0200

    vercel.json in /client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,5 @@
 import axios from 'axios'
-
-<<<<<<< HEAD
 export const request = axios.create({
-  baseURL: `e-commerce-vue-bilg.vercel.app`,
+  baseURL: `https://e-commerce-vue-server.vercel.app`,
   withCredentials: true,
 })
-=======
-export const request = axios.create({ baseURL: `https://e-commerce-vue-bilg.vercel.app`, withCredentials: true })
->>>>>>> 1697fe8aec5b714007b675f41dd377ae66c39b69
