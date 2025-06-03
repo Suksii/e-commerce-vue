17f59ac3900eff2380a42cfb8a2f73d0d6010e43
Ramović Šućo <ramovic225@gmail.com>
2025-06-03 21:15:45 +0200

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
-<<<<<<< HEAD
-=======
 
->>>>>>> fe5b13f98b6108151c79a8244d70cc08fc73634b
 export const request = axios.create({
   baseURL: `https://e-commerce-vue-server.vercel.app`,
   withCredentials: true,
