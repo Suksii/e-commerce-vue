2040f3e049ae83a3576639da6279f038c450b7b2
unknown <ramovic225@gmail.com>
2025-06-04 21:21:46 +0200

    cors error fixed

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,4 @@
-import app from "..";
-
-export const handler = async (req, res) => {
+import app from "../index.js";
+export default async function handler(req, res) {
   await app(req, res);
-};
+}
